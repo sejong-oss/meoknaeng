32800966411953cd1c1c6731337b4931e91d9232
yunseo225 <0225jys@gmail.com>
2026-05-20 15:15:03 +0900

    feat: 페이지별 브라우저 탭 타이틀 설정 (#94)
    * feat: 페이지별 브라우저 탭 타이틀 설정
    
    * refactor: 탭 타이틀 적용 방식 및 페이지명 수정
    
    * chore: lint 통과하도록 수정

--- frontend/src/pages/FeedWrite.jsx
+++ frontend/src/pages/FeedWrite.jsx
@@ -1,3 +1,10 @@
+import { SITE_NAME } from "@/lib/constants.js";
+
 export default function FeedWrite() {
-    return <div>레시피 공유 작성</div>;
+    return (
+        <>
+            <title>{`게시글 작성 | ${SITE_NAME}`}</title>
+            <div>레시피 공유 작성</div>
+        </>
+    );
 }
